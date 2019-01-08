04abd937d1ded7c5faf46ec2eed5a973bf3b3b07
Kaitlin A Kimmel <kimme099@umn201858628.local>
2019-01-08 11:47:37 -0600

    I created a dataframe to easily subset out functional groups for the within and between functional group calculations. I then calculated the sums of pairwise interactions along with the within and between functional group sums.

--- R/DIModels_BioCONTisN.R
+++ R/DIModels_BioCONTisN.R
@@ -9,7 +9,7 @@
 ######################################################################
 ## Methods: I will be using Diversity- Interaction modeling 
 # (refs: Kirwan et al 2009 Ecology & Connolly et al 2013 Journal of Ecology)
-# Some of the below code is borrowed from Forest Isbell 
+# Some of the below code is borrowed from Connolley et al 2013 & Forest Isbell 
 
 # DI models predict an ecosystem function (in our case, community tissue nitrogen)
 # by modeling identity effects, diversity effects, and environmental context effects
@@ -42,10 +42,70 @@ library(tidyr)
 # Data 
 # dataset with % n and % c from aboveground biomass
 TisN <- read.csv(here("data", "TisN_clean.csv"),row.names = 1)
-names(TisN) <- c("Year", "Date", "Plot", "Ring", "CO2Trt", "NTrt", "SR", "FGR",
-                 "Exp", "monospecies", "monoFunGroup", "WaterTrt", "TempTrt", "Comments",
-                 "Carbon", "Nitrogen", "CNRatio")
+# Experimental design of BioCON
 ExpDes <- read.csv(here("data", "BioCONExpDes.csv"))
+
+###########################################################################
+# Create a datafame with the functional groups to be used later
+fungroup <- data.frame(Species = as.character(colnames(ExpDes)[3:18]))
+fungroup$fungroup <- c("F", "C3", "L", "C4", "F", "F", "C4", "C3", "C3", 
+                        "L", "L", "L", "C3", "C4", "F", "C4")
+df1 <- data.frame(Sp1= character(), Sp2 = character(), Fg1 = character(), Fg2 = character())
+for(i in 1:(nrow(fungroup)-1)){
+  Sp1 = as.character(rep(fungroup$Species[i],16-i))
+  Sp2 = as.character(fungroup$Species[c((i+1):nrow(fungroup))])
+  Fg1 = as.character(rep(fungroup$fungroup[i], 16-i))
+  Fg2 = as.character(fungroup$fungroup[c((i+1):nrow(fungroup))])
+  x <- cbind(Sp1, Sp2, Fg1, Fg2)
+  df1 <- rbind(df1, x)
+}
+df1$colnums <- seq(23,142, by = 1)
+
+for(i in 1:nrow(df1)){
+  if(df1$Fg1[i] == df1$Fg2[i]){
+    df1$Between[i] = 0
+    if(df1$Fg1[i] == "F"){
+      df1$Within[i] = 1
+    }
+    if(df1$Fg1[i] == "L"){
+      df1$Within[i] = 2
+    }
+    if(df1$Fg1[i] == "C3"){
+      df1$Within[i] = 3
+    }
+    if(df1$Fg1[i] == "C4"){
+      df1$Within[i] = 4
+    }
+  }
+  else{
+    df1$Within[i] = 0
+  if(df1$Fg1[i] == "F" & df1$Fg2[i] == "L" | 
+     df1$Fg1[i] == "L" & df1$Fg2[i] == "F"){
+      df1$Between[i] = 12
+  }
+  if(df1$Fg1[i] == "F" & df1$Fg2[i] == "C3" | 
+     df1$Fg1[i] == "C3" & df1$Fg2[i] == "F"){
+    df1$Between[i] = 13
+  }
+  if(df1$Fg1[i] == "F" & df1$Fg2[i] == "C4" | 
+     df1$Fg1[i] == "C4" & df1$Fg2[i] == "F"){
+    df1$Between[i] = 14
+  }
+  if(df1$Fg1[i] == "L" & df1$Fg2[i] == "C3" | 
+     df1$Fg1[i] == "C3" & df1$Fg2[i] == "L"){
+    df1$Between[i] = 23
+  }
+  if(df1$Fg1[i] == "L" & df1$Fg2[i] == "C4" | 
+     df1$Fg1[i] == "C4" & df1$Fg2[i] == "L"){
+    df1$Between[i] = 24
+  }
+  if(df1$Fg1[i] == "C3" & df1$Fg2[i] == "C4" | 
+     df1$Fg1[i] == "C4" & df1$Fg2[i] == "C3"){
+    df1$Between[i] = 34
+  }
+  }
+}
+
 
 
 ##########################################################################
@@ -55,10 +115,74 @@ ExpDes <- read.csv(here("data", "BioCONExpDes.csv"))
 ########################################################################
 
 # Subset TisN for last year of experiment, plot, ring, SR, %N, CO2, & N treatments
-SIMod.df <- TisN[TisN$ExpYear == max(TisN$ExpYear), c(3:7,16)]
-SIMod.df <- merge(SIMod.df, ExpDes, by = c("Plot", "Ring"))
+df <- TisN[TisN$ExpYear == max(TisN$ExpYear), c(3:7,16)]
+df <- merge(df, ExpDes, by = c("Plot", "Ring"))
 # Check the SR column = number of planted species
-which(SIMod.df$SR != rowSums(SIMod.df[,c(7:22)])) # Equals 0 - all good!
-SIMod.df[c(7:22)] <- SIMod.df[c(7:22)]/SIMod.df$SR
+which(df$SR != rowSums(df[,c(7:22)])) # Equals 0 - all good!
+df[c(7:22)] <- df[c(7:22)]/df$SR
 # Need to put Tissue N as last column
-SIMod.df <- SIMod.df[,c(1:5,7:22,6)]
+df <- df[,c(1:5,7:22,6)]
+# Rename species to P1:16
+colnames(df)[6:21] <- paste("P", 1:16, sep = "")
+
+# Below code is adapted from Connolly et al 2013 and Forest Isbell
+
+#################################################################
+# Data manipulation
+# Function to create pairwise interactions
+# X is the data; NC is the number of columns before species proportions
+# Ecosystem function is the last column (here, tissue N)
+#################################################################
+
+PairwiseInt <- function(x,nc){
+  vk <- NULL
+  for (k in 1:nrow(x)){
+    vi <- NULL
+    for (i in (nc+1):(ncol(x)-2)){
+      vj <- NULL
+      
+      for (j in (i+1):(ncol(x)-1)) {
+        x[k,i]*x[k,j] -> temp
+        names(temp)<- paste(colnames(x)[i], colnames(x)[j], sep=".")	
+        vj=c(vj, temp)
+      }
+      vi <- c(vi, vj)
+      vi		
+    }
+    vk <- rbind(vk, vi)
+    row.names(vk) <- c(1:k)	
+    print(paste("row", k, "done"))
+  }
+  x <- data.frame(x, vk)
+  return(x)
+  
+}
+
+#############################################################
+# Apply pairwise interaction function - assuming theta = 1
+df <- PairwiseInt(df,5)
+# Rename interaction terms to PP1:120
+colnames(df)[23:142] <- paste("PP", 1:120, sep="")
+
+## Compute sum of pairwise interactions
+df$PPsum <- apply(df[23:142], MARGIN=1, FUN=sum)
+
+#########################################################
+## Compute within and between functional group interaction sums
+# FG1 = Forbs; FG2 = Legumes; FG3= C3; FG4 = C4
+df$PPwfg1 <- apply(df[df1$colnums[which(df1$Within == 1)]],MARGIN=1,FUN=sum)
+df$PPwfg2 <- apply(df[df1$colnums[which(df1$Within == 2)]],MARGIN=1,FUN=sum)
+df$PPwfg3 <- apply(df[df1$colnums[which(df1$Within == 3)]],MARGIN=1,FUN=sum)
+df$PPwfg4 <- apply(df[df1$colnums[which(df1$Within == 4)]],MARGIN=1,FUN=sum)
+df$PPbfg12 <- apply(df[df1$colnums[which(df1$Between == 12)]],MARGIN=1,FUN=sum)
+df$PPbfg13 <- apply(df[df1$colnums[which(df1$Between == 13)]],MARGIN=1,FUN=sum)
+df$PPbfg14 <- apply(df[df1$colnums[which(df1$Between == 14)]],MARGIN=1,FUN=sum)
+df$PPbfg23 <- apply(df[df1$colnums[which(df1$Between == 23)]],MARGIN=1,FUN=sum)
+df$PPbfg24 <- apply(df[df1$colnums[which(df1$Between == 24)]],MARGIN=1,FUN=sum)
+df$PPbfg34 <- apply(df[df1$colnums[which(df1$Between == 34)]],MARGIN=1,FUN=sum)
+
+# Test that PP variables sum to df$PPsum to test arithmetic
+test= df$PPsum-df$PPwfg1 -df$PPwfg2 -df$PPwfg3 -df$PPwfg4-df$PPbfg12-df$PPbfg13 -df$PPbfg14 -df$PPbfg23 -df$PPbfg24 -df$PPbfg34 
+sum(test)
+max(df$PPsum)
+
